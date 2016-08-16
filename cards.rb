4a4908ee88bdf2d33d685f2f7355743b1af65b62
gionaufal <g10v45+github@gmail.com>
2016-08-15 21:36:59 -0300

    update cards.rb - add option 3

--- cards.rb
+++ cards.rb
@@ -1,50 +1,14 @@
 puts 'Bem vindo ao Card Systems'
 
-# Essa é uma possível maneira de resolver o problema. Porém, ela é mais custosa para memória
-#
-#opção 1
-
-# def intro
-#   puts
-#   puts 'Digite a opção desejada'
-#   puts '[1] Inserir uma expressão'
-#   puts '[2] Sair'
-#   get_option
-# end
-#
-# def get_option
-#   option = gets.to_i
-#   if option == 1
-#     puts 'Insira uma expressão em inglês'
-#     eng = gets.chomp
-#     puts
-#     puts 'Insira a expressão em português'
-#     port = gets.chomp
-#     puts
-#     puts "#{eng} -> #{port}"
-#     intro
-#   elsif option == 2
-#     puts 'Adeus'
-#     exit
-#   else
-#     puts 'Opção inválida'
-#     puts 'digite 1 ou 2'
-#     get_option
-#   end
-# end
-#
-# intro
-#
-#opção 2
-
 option = 0
 cards = []
 
-while option != 2
+while option != 3
   puts
   puts 'Digite a opção desejada'
   puts '[1] Inserir uma expressão'
-  puts '[2] Sair'
+  puts '[2] Ver cards'
+  puts '[3] Sair'
 
   option = gets.to_i
   if option == 1
@@ -54,14 +18,21 @@ while option != 2
     puts 'Insira a expressão em português'
     port = gets.chomp
     puts
-    card = [eng, port]
+    card = "#{eng} -> #{port}"
     cards << card
-    puts "#{eng} -> #{port}"
+    puts card
     puts 'Card inseridos'
-  elsif option == 2
-    puts 'Seus cards: ' + cards.to_s
+  elsif option == 3
+    puts 'Seus cards: '
+    cards.each do |card|
+      puts card
+    end
     puts 'Adeus'
     exit
+  elsif option == 2
+    cards.each do |card|
+      puts card
+    end
   else
     puts 'Opção inválida'
     puts 'digite 1 ou 2'
